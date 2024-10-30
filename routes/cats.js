8c964a47b01b7517079e578693decc9a47c8309f
Franklin Kong <franklinkong981@gmail.com>
2024-10-30 13:48:52 -0700

    Finished first OOP model

--- routes/cats.js
+++ routes/cats.js
@@ -1,13 +1,64 @@
 const express = require("express");
 const db = require("../db");
+const Cat = require("../models/cat");
 
 const router = new express.Router();
 
 /** get all cats: [{id, name, age}, ...] */
 router.get("/", async function (req, res, next) {
-  let result = await db.query("SELECT * FROM cats");
-  let cats = result.rows;
-  return res.json(cats);
+  try {
+    const cats = await Cat.getAll();
+    return res.json(cats);
+  } catch(e) {
+    return next(e);
+  }
+});
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    const cat = await Cat.getById(req.params.id);
+    return res.json(cat);
+  } catch(e) {
+    return next(e);
+  }
+});
+
+router.post('/', async (req, res, next) => {
+  try {
+    const {name, age} = req.body;
+    const cat = await Cat.create(name, age);
+    return res.json(cat);
+  } catch(e) {
+    return next(e);
+  }
+}); 
+
+router.delete('/:id', async (req, res, next) => {
+  try {
+    await Cat.delete(req.params.id);
+    return res.json({msg: "Deleted"});
+  } catch(e) {
+    return next(e);
+  }
+});
+
+router.put('/:id', async (req, res, next) => {
+  try {
+    const {name, age} = req.body;
+    const cat = await Cat.update(req.params.id, name, age);
+    return res.json(cat);
+  } catch(e) {
+    return next(e);
+  }
+});
+
+router.patch('/:id', async (req, res, next) => {
+  try {
+    const cat = await Cat.makeOlder(req.params.id);
+    return res.json(cat);
+  } catch(e) {
+    return next(e);
+  }
 });
 
 module.exports = router;

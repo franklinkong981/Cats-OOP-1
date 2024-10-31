d060204007259c7317c97fa3512f07008f2bfc13
Franklin Kong <franklinkong981@gmail.com>
2024-10-31 13:40:34 -0700

    Implemented smarter OOP model with Dog class

--- routes/dogs.js
+++ routes/dogs.js
@@ -1,10 +1,62 @@
 const express = require("express");
 const router = new express.Router();
 const db = require("../db");
+const Dog = require("../models/dog");
 
 router.get("/", async function (req, res, next) {
-  let dogs = await db.query(`SELECT * FROM dogs`);
-  return res.json(dogs.rows);
+  let dogs = await Dog.getAll();
+  return res.json(dogs);
+});
+
+router.get("/:id", async function (req, res, next) {
+  try {
+    let dog = await Dog.getById(req.params.id);
+    return res.json(dog);
+  } catch(e) {
+    return next(e);
+  }
+});
+
+router.post("/", async function (req, res, next) {
+  try {
+    const {name, age} = req.body;
+    let dog = await Dog.create(name, age);
+    return res.json(dog);
+  } catch(e) {
+    return next(e);
+  }
+});
+
+router.patch("/:id/age", async function (req, res, next) {
+  try {
+    let dog = await Dog.getById(req.params.id);
+    dog.age += 1;
+    await dog.save();
+    return res.json({dog});
+  } catch(e) {
+    return next(e);
+  }
+});
+
+router.patch("/:id/rename", async function (req, res, next) {
+  try {
+    let dog = await Dog.getById(req.params.id);
+    dog.name = req.body.name;
+    await dog.save();
+    return res.json({dog});
+  } catch(e) {
+    return next(e);
+  }
+});
+
+router.delete("/:id", async function (req, res, next) {
+  try {
+    let dog = await Dog.getById(req.params.id);
+    await dog.remove();
+    return res.json({msg: "deleted"});
+  } catch(e) {
+    return next(e);
+  }
 });
 
 module.exports = router;
